832a68494114ec2c862cfe135bf392c8e4ad26bd
DESKTOP-49DLLO2\benja <benjamin.strumeyer@gmail.com>
2023-06-16 01:44:54 -0400

    feat: animate menu items

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,6 +1,9 @@
 import './Menu.scss';
+import { useMediaQuery } from '../../hooks/useMatchMedia';
 
-import { RefObject } from 'react';
+import { RefObject, useRef } from 'react';
+import gsap from 'gsap';
+
 
 interface MenuProps {
   homeRef: RefObject<HTMLDivElement>;
@@ -9,6 +12,8 @@ interface MenuProps {
 
 const Menu = (props: MenuProps) => {
   const { homeRef, contactRef } = props;
+  const menuItemsRef = useRef<Array<HTMLDivElement | null>>([]);
+  const isMobile = useMediaQuery('(max-width: 1200px');
 
   const menu = [
     {
@@ -29,13 +34,69 @@ const Menu = (props: MenuProps) => {
     }
   };
 
+  const animateOnHover = (index: number) => {
+    const menuItem = menuItemsRef.current[index];
+    const content = menuItem?.querySelector('.Content');
+    const scaleFactor = isMobile ? 1.1 : 1.3;
+    const inverseScaleFactor = isMobile ? 0.909 : 0.7692;
+
+    if (menuItem && content) {
+      gsap.killTweensOf(menuItem);
+      gsap.killTweensOf(content);
+
+      gsap.to(menuItem, {
+        scaleX: scaleFactor,
+        transformOrigin: 'center',
+        border: '1px solid #03FE97',
+        duration: 0.5,
+        backgroundColor: '#050F10',
+        translateX: '3vw',
+      });
+      gsap.to(content, {
+        color: '#03FE97',
+        duration: 0.5,
+        scaleX: inverseScaleFactor,
+      });
+    }
+  };
+
+  const resetAnimation = (index: number) => {
+    const menuItem = menuItemsRef.current[index];
+    const content = menuItem?.querySelector('.Content');
+    if (menuItem && content) {
+      gsap.killTweensOf(menuItem);
+      gsap.killTweensOf(content);
+
+      gsap.to(menuItem, {
+        scaleX: 1,
+        duration: 1,
+        border: '1px solid #12101B',
+        backgroundColor: '#12101b',
+        translateX: 0,
+      });
+      gsap.to(content, {
+        color: '#FFFFFF',
+        duration: 1,
+        scaleX: 1,
+      });
+    }
+  };
+
   return (
     <div className='Menu'>
       {menu.map((item, index) => (
-        <div onClick={() => scrollToRef(item.ref)}>
-          <div className='Index'>//0{index}.</div>
-          <div>{item.title}</div>
-        </div>)
+        <div
+          onMouseEnter={() => animateOnHover(index)}
+          onMouseLeave={() => resetAnimation(index)}
+          ref={(el) => (menuItemsRef.current[index] = el)}
+          onClick={() => scrollToRef(item.ref)}
+          className="MenuItem">
+          <div className="Content">
+            <div className='Index'>//0{index}.</div>
+            <div className="Title">{item.title}</div>
+          </div>
+        </div>
+      )
       )}
     </div>
   );
